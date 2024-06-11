dc196dc7a77059eeba87ac1d597e81d9bcb50f7d
parthshah1 <parthhshah171@gmail.com>
2024-06-11 16:06:51 -0400

    Refactoring lotus-2 node

--- lotus/start_scripts/lotus-follower.sh
+++ lotus/start_scripts/lotus-follower.sh
@@ -9,8 +9,7 @@ chain_1=$(curl 10.20.20.21/chains | jq -r '.[]')
 curl 10.20.20.21/$chain_1/info | jq -c  > /go/lotus-local-net/devgen/chain_info
 export DRAND_CHAIN_INFO=/go/lotus-local-net/devgen/chain_info
 
-/go/lotus-local-net/./lotus daemon  --genesis=/go/lotus-local-net/devgen/devgen.car &
-/go/lotus-local-net/./lotus wait-api
+/go/lotus-local-net/./lotus daemon  --genesis=/go/lotus-local-net/devgen/devgen.car --bootstrap=false &
 /go/lotus-local-net/./lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01001.key
 /go/lotus-local-net/./lotus-miner init --genesis-miner --actor=t01001 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01001.json --nosync
 /go/lotus-local-net/./lotus-miner run --nosync 
@@ -23,4 +22,5 @@ done
 ipv4addr=$(head -n 1 $file_to_check)
 echo $ipv4addr
 /go/lotus-local-net/./lotus net connect $ipv4addr
+/go/lotus-local-net/.lotus sync status
 sleep infinity
