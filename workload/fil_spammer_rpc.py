537357a6b7ed852ddd0e8f0fdea7cf6afc2dc268
Chang Xiao <chang.xiao@antithesis.com>
2024-06-28 22:35:37 -0400

    getting transfer to work by RPC

--- workload/fil_spammer_rpc.py
+++ workload/fil_spammer_rpc.py
@@ -6,14 +6,14 @@
 4. Message in the workload that actually start the fault
 '''
 
-import requests, json
+import requests, json, random
 # import json
 
 class fil_spammer_rpc():
     '''
     spamming fil across a local network
     '''
-    def __init__(self, rpc_url:str, auth_token:str, wallet_filepath:str):
+    def __init__(self, rpc_url:str, auth_token:str, common_mount_path:str):
         '''
         The auth token should be generated from  the cli before starting the workload
         lotus auth create-token --perm <read,write,sign,admin>
@@ -22,7 +22,7 @@ class fil_spammer_rpc():
         self.auth_token = auth_token
         # url:1234/rpc/v0
         self.rpc_url = rpc_url
-        self.wallet_filepath = wallet_filepath
+        self.basepath = common_mount_path
 
     def do_request(self, method:str, payload:dict) -> dict:
         '''
@@ -65,34 +65,176 @@ class fil_spammer_rpc():
         '''
         Creating the initial development wallets for spamming
         '''
-        # lotus_node = 'lotus-1'
-        # with open('/etc/hostname', 'r') as fh:
-        #     lotus_node = fh.read()
 
-        wallet_list_path = f'{self.wallet_filepath}/wallet-{node}.txt'
+        wallet_list_path = f'{self.basepath}/wallet-{node}.json'
 
-        wallets_created = []
+        wallets_created = {}
         for _ in range(num_wallets):
-            wallet_created = self.create_wallet()
-            wallets_created.append(f'{wallet_created}\n')
-
-        print(f'Created {len(wallets_created)} wallets on {node}')
-        print(wallets_created)
+            id = self.create_wallet()
+            wallets_created[id] = ''
 
         with open(wallet_list_path, 'w') as fh:
-            fh.writelines(wallets_created)
+            fh.write(json.dumps(wallets_created))
+
+        print(f'Created {num_wallets} wallets on {node}')
+        print(wallets_created)
+
+    def get_wallet_pks(self):
+        '''
+        Getting wallet private keys so we can use them in transactions
+        '''
+        # wallet_list_path = f'{self.basepath}/wallet-{node}.txt'
+
 
     def read_wallets(self, node:str = 'lotus-1') -> str:
         wallets = []
-        with open(f'{self.wallet_filepath}/wallet-{node}.txt', 'r') as fh:
-            wallets = fh.readlines()
+        with open(f'{self.basepath}/wallet-{node}.json', 'r') as fh:
+            wallets = json.load(fh)
 
         return wallets
 
-    def transfer_from_genesis_wallet(genesis_wallet_id:str, destination_wallet:str, amount:int):
 
-        pass
+        cid = self.get_chainhead()
+        if (not bool(cid)):
+            print('Failed to get CID from chainhead RPC call')
+            return
 
+        method = 'Filecoin.MpoolPushUntrusted'
+        payload = json.dumps({
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method,
+            "params": [
+                {
+                    "Message": {
+                    # "Version": 42,
+                    "To": wallet_to_id,
+                    "From": wallet_from_id,
+                    # "Nonce": 42,
+                    "Value": amount,
+                    "GasLimit": 10000000,
+                    "GasFeeCap": "10000000",
+                    "GasPremium": "0",
+                    "Method": 1,
+                    "Params": wallet_from_pk,
+                    "CID": {
+                        "/": cid,
+                    }
+                    },
+                    "Signature": {
+                    "Type": 2,
+                    "Data": wallet_from_pk
+                    },
+                    "CID": {
+                    "/": cid,
+                    }
+                }
+            ]
+        })
+        res = self.do_request('post', payload)
+
+        if res['response'].status_code != 200:
+            print(f'Bad response from {method}')
+            print(res)
+            # return False
+
+        return res
+
+    def _estimate_message_gas(self, wallet_from_id:str, wallet_to_id:str, amount:str) -> dict:
+        '''
+        Estiamte gas for a mPool message
+        return a dictionary with
+        Nonce, Value, GasLimit, GasFeeCap, GasPremium
+        '''
+        method = 'Filecoin.GasEstimateMessageGas'
+        payload = json.dumps({
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method,
+            "params": [
+                {
+                    # "Version": 42,
+                    "To": wallet_to_id,
+                    "From": wallet_from_id,
+                    "Value": amount,
+                    "GasLimit": 10000000,
+                    "Method": 0,
+                },
+                {
+                    "MaxFee": "0"
+                }, None
+            ]
+        })
+
+        res = self.do_request('post', payload)
+
+        if res['response'].status_code != 200:
+            print(f'Bad response from {method}')
+            # print(res)
+            return False
+
+        data = res['response'].json()
+        return data['result']
+
+    def transfer(self, wallet_from_id:str, wallet_from_pk:str, wallet_to_id: str, amount:str):
+
+        cid = self.get_chainhead()
+        if (not bool(cid)):
+            print('Failed to get CID from chainhead RPC call')
+            return
+
+        gas_info = self._estimate_message_gas(wallet_from_id, wallet_to_id, amount)
+        if (not bool(gas_info)):
+            print('Failed to get gas information to do MpoolPushMessage')
+            return
+
+        method = 'Filecoin.MpoolPushMessage'
+        payload = json.dumps({
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method,
+            "params": [
+                {
+                    # "Version": 42,
+                    "To": wallet_to_id,
+                    "From": wallet_from_id,
+                    "Value": amount,
+                    "GasLimit": gas_info['GasLimit'],
+                    "GasFeeCap": gas_info['GasFeeCap'],
+                    "GasPremium": gas_info['GasPremium'],
+                    "Method": 0,
+                    # "Params": wallet_from_pk,
+                    "Params": "",
+                    "CID": {
+                    "/": cid
+                    }
+                },
+                {
+                    "MaxFee": "0"
+                }
+            ]
+        })
+        res = self.do_request('post', payload)
+
+        if res['response'].status_code != 200:
+            print(f'Bad response from {method}')
+            print(res)
+            # return False
+        return res
+
+    def transfer_from_genesis(self, destination_wallet_id:str, amount:str):
+        genesis_wallet = self.get_genesis_wallet()
+        if not bool(genesis_wallet):
+            print('Unable to retrieve genesis wallet')
+            return
+
+        genesis_pk = self.get_wallet_pk(genesis_wallet)
+
+        print(f'Genesis wallet: {genesis_wallet}')
+        print(f'Genesis wallet pk: {genesis_pk}')
+        print(f'Destination wallet: {destination_wallet_id}')
+
+        return self.transfer(genesis_wallet, genesis_pk, destination_wallet_id, amount)
 
     def create_wallet(self) -> str:
         '''
@@ -104,7 +246,7 @@ class fil_spammer_rpc():
             "jsonrpc": "2.0",
             "id": "1",
             "method": method,
-            "params": ['secp256k1']
+            "params": [1]
         })
         res = self.do_request('post', payload)
 
@@ -115,6 +257,27 @@ class fil_spammer_rpc():
         data = res['response'].json()
 
         return data['result']
+    
+    def get_wallet_pk(self, wallet_id) -> str:
+        '''
+        Getting the private key for a wallet so we can use it to send transactions
+        '''
+        method = 'Filecoin.WalletExport'
+        payload = json.dumps({
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method,
+            "params": [wallet_id]
+        })
+        res = self.do_request('post', payload)
+
+        if res['response'].status_code != 200:
+            print(f'Bad response from {method}')
+            return False
+
+        data = res['response'].json()
+
+        return data['result']['PrivateKey']
 
     def get_genesis_wallet(self):
         '''
@@ -143,21 +306,62 @@ class fil_spammer_rpc():
 
         return genesis_wallet
 
+    def get_chainhead(self):
+        '''
+        This method doesn't need auth
+        @return cid that we can potentially use to push messages
+        '''
+        cid = ''
+        method = 'Filecoin.ChainHead'
+        payload = json.dumps({
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method
+        })
+        res = self.do_request('post', payload)
+
+        if res['response'].status_code != 200:
+            print(f'Bad response from {method}')
+            return False
+
+        response_body = res['response'].json()
+
+        if len(response_body['result']['Cids']) > 0:
+            cid = response_body['result']['Cids'][0]
+            # pretty bad here
+            if '/' in cid:
+                cid = cid['/']
+
+        return cid
+
 if __name__ == '__main__':
 
     lotus1 = 'http://10.20.20.24:1234/rpc/v0'
-    auth_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.svZiMvSuo3lOvOpL9ut_4ZZzS1CnIyayCNTRos8jReU'
     base_path = '/root/devgen'
+
+    auth_token = ''
+    with open(f'{base_path}/lotus-1-token.txt') as f:
+        auth_token = f.read()
+
+    auth_token = auth_token.rstrip()
+
+    print(f'Auth token: ({auth_token})')
 
     spammer = fil_spammer_rpc(lotus1, auth_token, base_path)
 
+    print('Genesis wallet')
     print(spammer.get_genesis_wallet())
-    # spammer.create_wallets()
 
+    # spammer.create_wallets()
 
     # wallets = spammer.read_wallets()
     # print(wallets)
 
+
+    # print(spammer.create_wallet())
+
+    res = spammer.transfer_from_genesis('t1y22of2obqrgrqnswiicjpwlwhtyb7szjkefhfii', '100000')
+    print(res)
+    print(res['response'].text)
+
     print('done')
-
-
