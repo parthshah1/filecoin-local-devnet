9a8f9cd68ba44d32e9984aa7b7515dced01de33b
parthshah1 <parthhshah171@gmail.com>
2024-07-10 16:18:47 -0400

    Updating the workload

--- workload/fil_spammer_rpc.py
+++ workload/fil_spammer_rpc.py
@@ -2,122 +2,56 @@ import requests
 import json
 import os
 import time
-import subprocess
 import random
 
-class fil_spammer_rpc():
-    def __init__(self, rpc_url:str, auth_token:str, common_mount_path:str):
-        self.auth_token = auth_token
+class FilSpammerRPC:
+    def __init__(self, rpc_url, token_path):
         self.rpc_url = rpc_url
-        self.basepath = common_mount_path
+        self.auth_token = self._read_auth_token(token_path)
 
-    def do_request(self, method:str, payload:dict) -> dict:
+    def _read_auth_token(self, token_path):
+        with open(token_path) as f:
+            auth_token = f.read().strip()
+        return auth_token
+
+    def do_request(self, method, payload):
         headers = {
             "Content-Type": "application/json",
             "Authorization": f'Bearer {self.auth_token}'
         }
+        response = requests.post(self.rpc_url, headers=headers, data=json.dumps(payload))
+        return response.json()
 
-        if method in ['get', 'post', 'put', 'delete', 'head', 'options']:
-            payload_mapping = {
-                'get': 'params',
-                'post': 'data',
-            }
-            kwargs = {}
-            if bool(payload):
-                if method in payload_mapping.keys():
-                    kwargs.update({payload_mapping[method]: payload})
-
-            func = getattr(requests, method)
-            response = func(self.rpc_url, headers=headers, **kwargs)
-            response_json = response.json()
-
-            return {
-                'request': {
-                    'url': self.rpc_url,
-                    'headers': headers,
-                    'payload': payload,
-                },
-                'response': response,
-                'response_json': response_json
-            }
-
-    def create_wallet(self) -> str:
+    def create_wallet(self):
         method = 'Filecoin.WalletNew'
-        payload = json.dumps({
+        payload = {
             "jsonrpc": "2.0",
             "id": "1",
             "method": method,
             "params": [1]
-        })
+        }
         res = self.do_request('post', payload)
-
-        if res['response'].status_code != 200:
-            print(f'Bad response from {method}')
-            print(res['response_json'])
-            return False
-
-        return res['response_json']['result']
+        if 'error' in res:
+            print(f'Error in {method}:', res['error'])
+            return None
+        return res['result']
 
     def get_chainhead(self):
         method = 'Filecoin.ChainHead'
-        payload = json.dumps({
+        payload = {
             "jsonrpc": "2.0",
             "id": "1",
             "method": method
-        })
+        }
         res = self.do_request('post', payload)
+        if 'error' in res:
+            print(f'Error in {method}:', res['error'])
+            return None
+        return res['result']['Cids'][0]['/']
 
-        if res['response'].status_code != 200:
-            print(f'Bad response from {method}')
-            print(res['response_json'])
-            return False
-
-        response_body = res['response_json']
-        if len(response_body['result']['Cids']) > 0:
-            cid = response_body['result']['Cids'][0]
-            if '/' in cid:
-                cid = cid['/']
-            return cid
-        return None
-
-    def get_genesis_wallet(self):
-        genesis_wallet = False
-        method = 'Filecoin.WalletList'
-        payload = json.dumps({
-            "jsonrpc": "2.0",
-            "id": "1",
-            "method": method
-        })
-        res = self.do_request('post', payload)
-
-        if res['response'].status_code != 200:
-            print(f'Bad response from {method}')
-            print(res['response_json'])
-            return False
-
-        example_non_genesis_wallet = "t1sua7sz4h43j3rhsh3x7f5ciysyck7bqzme3dtxy"
-        response_body = res['response_json']
-        for wallet in response_body['result']:
-            if len(wallet) > len(example_non_genesis_wallet):
-                genesis_wallet = wallet
-
-        return genesis_wallet
-
-    def transfer_from_genesis(self, genesis_wallet, destination_wallet_id:str, amount:str):
-        command = [
-            "./lotus-local-net/lotus",
-            "send",
-            "--from",
-            genesis_wallet,
-            destination_wallet_id,
-            amount
-        ]
-        result = subprocess.run(command, capture_output=True, text=True)
-        return result
-
-    def _estimate_message_gas(self, wallet_from_id:str, wallet_to_id:str, amount:str) -> dict:
+    def _estimate_message_gas(self, wallet_from_id, wallet_to_id, amount):
         method = 'Filecoin.GasEstimateMessageGas'
-        payload = json.dumps({
+        payload = {
             "jsonrpc": "2.0",
             "id": "1",
             "method": method,
@@ -133,30 +67,26 @@ class fil_spammer_rpc():
                     "MaxFee": "0"
                 }, None
             ]
-        })
-
+        }
         res = self.do_request('post', payload)
+        if 'error' in res:
+            print(f'Error in {method}:', res['error'])
+            return None
+        return res['result']
 
-        if res['response'].status_code != 200:
-            print(f'Bad response from {method}')
-            print(res['response_json'])
-            return False
-
-        return res['response_json']['result']
-
-    def push_message(self, wallet_from_id:str, wallet_to_id: str, amount:str, nonce:int):
+    def push_message(self, wallet_from_id, wallet_to_id, amount, nonce):
         cid = self.get_chainhead()
-        if not bool(cid):
+        if not cid:
             print('Failed to get CID from chainhead RPC call')
-            return
+            return None
 
         gas_info = self._estimate_message_gas(wallet_from_id, wallet_to_id, amount)
-        if not bool(gas_info):
+        if not gas_info:
             print('Failed to get gas information to do MpoolPushMessage')
-            return
+            return None
 
         method = 'Filecoin.MpoolPushMessage'
-        payload = json.dumps({
+        payload = {
             "jsonrpc": "2.0",
             "id": "1",
             "method": method,
@@ -180,119 +110,143 @@ class fil_spammer_rpc():
                     "MaxFee": "0"
                 }
             ]
-        })
+        }
         res = self.do_request('post', payload)
-
-        if res['response'].status_code != 200:
-            print(f'Bad response from {method}')
-            print(res['response_json'])
-            return False
+        if 'error' in res:
+            print(f'Error in {method}:', res['error'])
+            return None
         return res
 
-    def fuzz_push_message(self, wallet_from_id:str, wallet_to_id: str, amount:str, nonce:int):
-        # Define possible fuzzing scenarios
-        fuzz_cases = [
-            {"Nonce": -1},  # Invalid nonce
-            {"Value": "0"},  # Zero amount
-            {"Value": "-100"},  # Negative amount
-            {"Value": "1000000000000000000000"},  # Excessively high amount
-            {"GasLimit": "0"},  # Zero gas limit
-            {"GasLimit": "-1000"},  # Negative gas limit
-            {"To": "t1invalidwallet"},  # Invalid recipient address
-            {"GasFeeCap": "0"},  # Zero gas price
-            {"GasFeeCap": "-1000"},  # Negative gas price
-        ]
+    def get_genesis_wallet(self):
+        genesis_wallet = False
+        method = 'Filecoin.WalletList'
+        payload = {
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": method,
+            "params": []
+        }
+        res = self.do_request('post', payload)
+        if 'error' in res:
+            print(f'Error in {method}:', res['error'])
+            return False
 
-        for fault in fuzz_cases:  # Test each fault separately
-            cid = self.get_chainhead()
-            if not bool(cid):
-                print('Failed to get CID from chainhead RPC call')
-                continue
+        example_non_genesis_wallet = "t1sua7sz4h43j3rhsh3x7f5ciysyck7bqzme3dtxy"
+        response_body = res
+        for wallet in response_body['result']:
+            if len(wallet) > len(example_non_genesis_wallet):
+                genesis_wallet = wallet
 
-            gas_info = self._estimate_message_gas(wallet_from_id, wallet_to_id, amount)
-            if not bool(gas_info):
-                print('Failed to get gas information to do MpoolPushMessage')
-                continue
+        return genesis_wallet
 
-            method = 'Filecoin.MpoolPushMessage'
-            fuzz_payload = {
-                "Version": 0,
-                "To": wallet_to_id,
-                "From": wallet_from_id,
-                "Nonce": nonce,
-                "Value": amount,
-                "GasLimit": gas_info['GasLimit'],
-                "GasFeeCap": gas_info['GasFeeCap'],
-                "GasPremium": gas_info['GasPremium'],
-                "Method": 0,
-                "Params": "",
-                "CID": {"/": cid}
-            }
 
-            # Inject the fault
-            fuzz_payload.update(fault)
-
-            payload = json.dumps({
-                "jsonrpc": "2.0",
-                "id": "1",
-                "method": method,
-                "params": [fuzz_payload, {"MaxFee": "0"}]
-            })
-
-            res = self.do_request('post', payload)
-            print(f'Fuzzing with fault {fault}: response', res['response_json'])
-
-if __name__ == '__main__':
-    # Get environment variables
-    rpc_url = os.getenv('RPC_URL', 'http://10.20.20.24:1234')
-    token_path = os.getenv('TOKEN_PATH', '/root/devgen/jwt')
-    num_wallets = int(os.getenv('NUM_WALLETS', 10))
-    num_transactions = int(os.getenv('NUM_TRANSACTIONS', 100))
-
-    with open(token_path) as f:
-        auth_token = f.read().strip()
-
-    spammer = fil_spammer_rpc(rpc_url, auth_token, os.path.dirname(token_path))
-
-    # Create wallets
+def create_wallets(spammer, num_wallets):
     wallets = []
     for _ in range(num_wallets):
         wallet_id = spammer.create_wallet()
-        wallets.append(wallet_id)
+        if wallet_id:
+            wallets.append(wallet_id)
+    return wallets
 
-    print(f'Created wallets: {wallets}')
-
-    # Get the genesis wallet
-    genesis_wallet = spammer.get_genesis_wallet()
-    if not genesis_wallet:
-        print('Unable to retrieve genesis wallet')
-        exit(1)
-    print(f'Genesis wallet: {genesis_wallet}')
-
-    # Distribute FIL from genesis wallet to created wallets
+def transfer_funds(spammer, genesis_wallet, wallets):
     for wallet_id in wallets:
-        res = spammer.transfer_from_genesis(genesis_wallet, wallet_id, '1000')
-        print(f'Transfer to {wallet_id} response:', res.stdout)
-
-    # Sleep for a while to ensure the funds are properly transferred
-    time.sleep(15)
-
-    # Get and print the ChainHead CID
-    chainhead_cid = spammer.get_chainhead()
-    print(f'ChainHead CID: {chainhead_cid}')
-
-    # Send multiple transactions
-    nonce = 0
-    for i in range(num_transactions):
-        from_wallet = wallets[i % len(wallets)]
-        to_wallet = wallets[(i + 1) % len(wallets)]
-        amount = str(100 + (i % 100))  # Example varying amounts
-
-        # Randomly decide whether to fuzz or send a good transaction
-        if random.random() < 0.5:  # 50% chance to fuzz
-            spammer.fuzz_push_message(from_wallet, to_wallet, amount, nonce)
+        res = spammer.push_message(genesis_wallet, wallet_id, '100', 0)
+        if res:
+            print(f'Transferred 100 FIL from {genesis_wallet} to {wallet_id}')
         else:
-            res = spammer.push_message(from_wallet, to_wallet, amount, nonce)
-            print(f'Push message {i+1} from {from_wallet} to {to_wallet} response:', res['response_json'])
+            print(f'Failed to transfer FIL to {wallet_id}')
 
-        nonce += 1
+def transfer_between_wallets(spammer, wallets):
+    with open('/dev/urandom', 'rb') as f:
+        random_bytes = f.read(1)
+        seed = int.from_bytes(random_bytes, byteorder='big')
+        print(f'Getting a random SEED {seed}')
+        random.seed(seed)
+
+    for _ in range(10):  # Performing 10 random transfers
+        from_wallet = random.choice(wallets)
+        to_wallet = random.choice(wallets)
+        while to_wallet == from_wallet:
+            to_wallet = random.choice(wallets)
+        res = spammer.push_message(from_wallet, to_wallet, '1', 0)
+        if res:
+            print(f'Transferred 1 FIL from {from_wallet} to {to_wallet}')
+        else:
+            print(f'Failed to transfer FIL from {from_wallet} to {to_wallet}')
+
+if __name__ == '__main__':
+    BASE_PATH1 = '/root/devgen'
+    BASE_PATH2 = '/root/devgen'
+    TOKEN_LOTUS_1 = 'token'
+    TOKEN_LOTUS_2 = 'token'
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "step",
+        type=str,
+        help="the workload step to run",
+        choices=[
+            "1_create_wallets",
+            "2_transfer_funds",
+            "3_transfer_between_wallets"
+        ],
+        default="1_create_wallets",
+    )
+
+    args = parser.parse_args()
+
+    lotus1_rpc = os.getenv("RPC_LOTUS1")
+    lotus2_rpc = os.getenv("RPC_LOTUS2")
+
+    if not lotus1_rpc or not lotus2_rpc:
+        print('Workload cannot start, missing environment variable RPC_LOTUS1 or RPC_LOTUS2')
+        exit(2)
+
+    lotus1_token = FilSpammerRPC._read_auth_token(os.path.expanduser(f'{BASE_PATH1}/{TOKEN_LOTUS_1}'))
+    lotus2_token = FilSpammerRPC._read_auth_token(os.path.expanduser(f'{BASE_PATH2}/{TOKEN_LOTUS_2}'))
+
+    if not lotus1_token or not lotus2_token:
+        print('Workload cannot start, unable to fetch auth tokens from Lotus nodes, make sure they are generated')
+        exit(3)
+
+    spammer1 = FilSpammerRPC(lotus1_rpc, lotus1_token)
+    spammer2 = FilSpammerRPC(lotus2_rpc, lotus2_token)
+
+    if args.step == '1_create_wallets':
+        print('Executing step 1, creating wallets on lotus nodes')
+        wallets1 = create_wallets(spammer1, 10)
+        wallets2 = create_wallets(spammer2, 10)
+        with open(f'{BASE_PATH1}/wallets1.json', 'w') as f:
+            json.dump(wallets1, f)
+        with open(f'{BASE_PATH2}/wallets2.json', 'w') as f:
+            json.dump(wallets2, f)
+        print(f'Created wallets for {lotus1_rpc}: {wallets1}')
+        print(f'Created wallets for {lotus2_rpc}: {wallets2}')
+    elif args.step == '2_transfer_funds':
+        print('Executing step 2, transferring funds on lotus nodes')
+        genesis_wallet1 = spammer1.get_genesis_wallet()
+        genesis_wallet2 = spammer2.get_genesis_wallet()
+        if not genesis_wallet1 or not genesis_wallet2:
+            print('Unable to retrieve genesis wallet')
+            exit(4)
+        print(f'Genesis wallet for {lotus1_rpc}: {genesis_wallet1}')
+        print(f'Genesis wallet for {lotus2_rpc}: {genesis_wallet2}')
+        with open(f'{BASE_PATH1}/wallets1.json', 'r') as f:
+            wallets1 = json.load(f)
+        with open(f'{BASE_PATH2}/wallets2.json', 'r') as f:
+            wallets2 = json.load(f)
+        transfer_funds(spammer1, genesis_wallet1, wallets1)
+        transfer_funds(spammer2, genesis_wallet2, wallets2)
+        print(f'Transferred funds for {lotus1_rpc}')
+        print(f'Transferred funds for {lotus2_rpc}')
+    elif args.step == '3_transfer_between_wallets':
+        print('Executing step 3, transferring funds between wallets on lotus nodes')
+        with open(f'{BASE_PATH1}/wallets1.json', 'r') as f:
+            wallets1 = json.load(f)
+        with open(f'{BASE_PATH2}/wallets2.json', 'r') as f:
+            wallets2 = json.load(f)
+        transfer_between_wallets(spammer1, wallets1)
+        transfer_between_wallets(spammer2, wallets2)
+        print(f'Transferred funds between wallets for {lotus1_rpc}')
+        print(f'Transferred funds between wallets for {lotus2_rpc}')
