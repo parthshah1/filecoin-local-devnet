5160f2694b2460b6aa1b49ccd14346ba735ace79
parthshah1 <parthhshah171@gmail.com>
2024-09-06 13:48:52 -0400

    Fixing

--- lotus/start_scripts/lotus-leader.sh
+++ lotus/start_scripts/lotus-leader.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 sleep 60
-while true; do
-    # Extract the first element from the JSON array and store it in a variable
-    chain_1=$(curl 10.20.20.21/info | jq -r '.[]')
-    echo "Chain Value: $chain_1"
-    # Check if chain is not empty
-    if [ -n "$chain_1" ] ; then
-        echo "Received data: $chain_1"
-        break
-    else
-        echo "No data received, retrying..."
-    fi
-
-    # Optionally, sleep for a short period to avoid spamming the server
-    sleep 1s
-done
-
-curl 10.20.20.21/$chain_1/info | jq -c > /opt/lotus_transformed/customer/devgen/chain_info
+curl 10.20.20.21/info | jq -c > /opt/lotus_transformed/customer/devgen/chain_info
 export DRAND_CHAIN_INFO=/opt/lotus_transformed/customer/devgen/chain_info
 
 ./lotus daemon --lotus-make-genesis=/opt/lotus_transformed/customer/devgen/devgen.car --genesis-template=/opt/lotus_transformed/customer/localnet.json --bootstrap=false --config=/opt/lotus_transformed/customer/devgen/config.toml &
